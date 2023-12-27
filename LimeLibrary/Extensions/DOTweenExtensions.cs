7a5514b630219d4648e04550d709aedcd3e8fee0
harada_takumi <limegame1004@gmail.com>
2023-12-27 22:04:25 +0900

    Add: DOTweenのToUniTask関数のTweenCancelBehaviourの初期値をKillAndCancelAwaitにする拡張関数追加

--- LimeLibrary/Extensions/DOTweenExtensions.cs
+++ LimeLibrary/Extensions/DOTweenExtensions.cs
@@ -1,5 +1,9 @@
 #if LIME_DOTWEEN
+using System.Threading;
 using DG.Tweening;
+#if LIME_UNITASK
+using Cysharp.Threading.Tasks;
+#endif
 
 namespace LimeLibrary.Extensions {
 
@@ -14,6 +18,18 @@ public static class DOTweenExtensions {
   public static void KillIfActive(this Tween t, bool complete = false) {
     if (t.IsActive()) t.Kill(complete);
   }
+
+#if UNITASK_DOTWEEN_SUPPORT
+  /// <summary>
+  /// TweenのUniTask変換
+  /// TweenCancelBehaviourはKillAndCancelAwait指定
+  /// </summary>
+  /// <param name="t"></param>
+  /// <param name="cancellationToken"></param>
+  public static async UniTask ToUniTask(this Tween t, CancellationToken cancellationToken = default) {
+    await t.ToUniTask(TweenCancelBehaviour.KillAndCancelAwait, cancellationToken);
+  }
+#endif
 }
 
 }
