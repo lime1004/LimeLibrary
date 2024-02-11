508823d75e0143d67ac8d2a926e8850d44162bd2
harada_takumi <limegame1004@gmail.com>
2024-02-11 22:40:45 +0900

    Add: IInstantiatableUiElementsを追加

---
+++ LimeLibrary/Scripts/UI/IInstantiatableUiElements.cs
@@ -0,0 +1,19 @@
+﻿using LimeLibrary.UI.View;
+using LimeLibrary.Utility;
+using UnityEngine;
+
+namespace LimeLibrary.UI {
+
+public interface IInstantiatableUiElements<out T> where T : MonoBehaviour {
+  public T Self { get; }
+
+  public T Instantiate(IUIView parentView) {
+    var instantiateObject = UnityUtility.Instantiate(Self, Self.transform.parent);
+    Setup(parentView);
+    return instantiateObject;
+  }
+
+  public void Setup(IUIView parentView);
+}
+
+}
