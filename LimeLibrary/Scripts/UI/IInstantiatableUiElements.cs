8fff0b2367856ac5111d73e1df47377ea46d958a
harada_takumi <limegame1004@gmail.com>
2024-02-11 23:31:51 +0900

    Fix: IInstantiatableUiElementsの改良

--- LimeLibrary/Scripts/UI/IInstantiatableUiElements.cs
+++ LimeLibrary/Scripts/UI/IInstantiatableUiElements.cs
@@ -11,6 +11,7 @@ public interface IInstantiatableUiElements<out T> where T : MonoBehaviour {
 public static class InstantiatableUiElementsExtensions {
   public static T Instantiate<T>(this IInstantiatableUiElements<T> instantiatable, IUIView parentView) where T : MonoBehaviour, IInstantiatableUiElements<T> {
     var instantiateObject = Object.Instantiate(instantiatable.Self, instantiatable.Self.transform.parent);
+    instantiateObject.gameObject.SetActive(true);
     instantiateObject.Initialize(parentView);
     return instantiateObject;
   }
