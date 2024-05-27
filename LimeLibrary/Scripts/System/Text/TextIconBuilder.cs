4771bad1a9c77175d3fc71b8a3acc28d0e50ddde
harada_takumi <limegame1004@gmail.com>
2024-05-27 18:48:14 +0900

    Add: TextIconBuilderの追加

---
+++ LimeLibrary/Scripts/System/Text/TextIconBuilder.cs
@@ -0,0 +1,8 @@
+﻿namespace LimeLibrary.Text {
+
+public static class TextIconBuilder {
+  public static string BuildIconText(string name) => $"<sprite name=\"{name}\">";
+  public static string BuildIconText(int index) => $"<sprite={index}>";
+}
+
+}
