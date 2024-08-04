5c653914911b6d6030583f365ce9042cf24e2dd8
harada_takumi <limegame1004@gmail.com>
2024-08-04 10:12:51 +0900

    Add: RunHandlingErrorForgetの追加

--- LimeLibrary/Scripts/Extensions/UniTaskExtensions.cs
+++ LimeLibrary/Scripts/Extensions/UniTaskExtensions.cs
@@ -20,9 +20,14 @@ public static class UniTaskExtensions {
     }
   }
 
-  /// <summary>
-  /// OperationCanceledException以外の例外が発生した場合にAssertion.Assertを呼び出す
-  /// </summary>
+  public static async UniTaskVoid RunHandlingErrorForget(this UniTask task) {
+    try {
+      await task;
+    } catch (Exception e) when (e is not OperationCanceledException) {
+      Assertion.Assert(false, e);
+    }
+  }
+
   public static async UniTask<T> RunHandlingError<T>(this UniTask<T> task) {
     try {
       return await task;
@@ -30,6 +35,14 @@ public static class UniTaskExtensions {
       Assertion.Assert(false, e);
     }
     return default;
+  }
+
+  public static async UniTaskVoid RunHandlingErrorForget<T>(this UniTask<T> task) {
+    try {
+      await task;
+    } catch (Exception e) when (e is not OperationCanceledException) {
+      Assertion.Assert(false, e);
+    }
   }
 }
 
