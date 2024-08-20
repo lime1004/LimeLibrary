0bf89b7299b5a822f8373de251cd55aeee3fda94
harada_takumi <limegame1004@gmail.com>
2024-08-20 15:13:03 +0900

    Add: ProbabilityListのエディタ拡張用クラスを追加

---
+++ LimeLibrary/Scripts/Module/Probability/IHasProbabilityList.cs
@@ -0,0 +1,9 @@
+﻿#if LIME_ODIN_INSPECTOR
+namespace LimeLibrary.Module {
+
+public interface IHasProbabilityList<T> {
+  internal ProbabilityList<T> GetProbabilityList();
+}
+
+}
+#endif
---
+++ LimeLibrary/Scripts/Module/Probability/ProbabilityDragAndDrop.cs
@@ -0,0 +1,70 @@
+﻿#if LIME_ODIN_INSPECTOR
+using System.Collections.Generic;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace LimeLibrary.Module {
+
+#if UNITY_EDITOR
+public abstract class ProbabilityDragAndDrop<T> : OdinEditor {
+  private readonly List<T> _addContentList = new();
+
+  public override void OnInspectorGUI() {
+    var dropArea = GUILayoutUtility.GetRect(0.0f, GetRectHeight(), GUILayout.ExpandWidth(true));
+    GUI.Box(dropArea, $"Add to drag {typeof(T)}", EditorStyles.helpBox);
+    HandleDragAndDrop(dropArea, _addContentList);
+
+    if (_addContentList.Count > 0) {
+      foreach (var content in _addContentList) {
+        AddProbabilityList(content);
+      }
+      _addContentList.Clear();
+    }
+
+    base.OnInspectorGUI();
+  }
+
+  protected virtual float GetRectHeight() {
+    return 50f;
+  }
+
+  protected virtual Probability<T> CreateProbability(T content) {
+    return new Probability<T>(content, false, 1);
+  }
+
+  protected virtual void AddProbabilityList(T content) {
+    if (target is IHasProbabilityList<T> probabilityList) {
+      var probability = CreateProbability(content);
+      probabilityList.GetProbabilityList().Add(probability);
+    }
+  }
+
+  private static void HandleDragAndDrop(Rect dropArea, ICollection<T> list) {
+    var evt = UnityEngine.Event.current;
+    switch (evt.type) {
+    case EventType.DragUpdated:
+    case EventType.DragPerform:
+      if (!dropArea.Contains(evt.mousePosition))
+        return;
+
+      DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+
+      if (evt.type == EventType.DragPerform) {
+        DragAndDrop.AcceptDrag();
+
+        foreach (var draggedObject in DragAndDrop.objectReferences) {
+          if (draggedObject is T addObject) {
+            list.Add(addObject);
+          }
+        }
+      }
+      UnityEngine.Event.current.Use();
+      break;
+    }
+  }
+}
+#endif
+
+}
+#endif
