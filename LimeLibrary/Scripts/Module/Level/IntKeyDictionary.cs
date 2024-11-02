2fe8091d71c34a13f8dac23e2698a03b85d018d0
harada_takumi <limegame1004@gmail.com>
2024-11-02 15:13:11 +0900

    Add: IntKeyDictionaryの追加

---
+++ LimeLibrary/Scripts/Module/Level/IntKeyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace LimeLibrary.Module {
+
+public enum GetValueType {
+  Default,
+  Next,
+  Previous
+}
+
+[Serializable]
+public class IntKeyDictionary<T> : SerializedDictionary<int, T> {
+  public T GetValue(int key, GetValueType getValueType) {
+    if (TryGetValue(key, out var value)) {
+      return value;
+    }
+
+    switch (getValueType) {
+    case GetValueType.Next: {
+      int nextKey = Keys.Where(k => k > key).OrderBy(k => k).FirstOrDefault();
+      if (nextKey != 0 || ContainsKey(nextKey)) {
+        return this[nextKey];
+      }
+      return default;
+    }
+    case GetValueType.Previous: {
+      int previousKey = Keys.Where(k => k < key).OrderByDescending(k => k).FirstOrDefault();
+      if (previousKey != 0 || ContainsKey(previousKey)) {
+        return this[previousKey];
+      }
+      return default;
+    }
+    default:
+      return default;
+    }
+  }
+}
+
+}
--- LimeLibrary/Scripts/Module/Level/LevelValueDictionary.cs
+++ LimeLibrary/Scripts/Module/Level/LevelValueDictionary.cs
@@ -1,40 +1,8 @@
 ﻿using System;
-using System.Linq;
 
 namespace LimeLibrary.Module {
 
-public enum GetValueType {
-  Default,
-  Next,
-  Previous
-}
-
 [Serializable]
-public class LevelValueDictionary : SerializedDictionary<int, int> {
-  public int GetValue(int level, GetValueType getValueType, int defaultValue = 0) {
-    if (TryGetValue(level, out int value)) {
-      return value;
-    }
-
-    switch (getValueType) {
-    case GetValueType.Next: {
-      var nextKey = Keys.Where(k => k > level).OrderBy(k => k).FirstOrDefault();
-      if (nextKey != 0 || ContainsKey(nextKey)) {
-        return this[nextKey];
-      }
-      return defaultValue;
-    }
-    case GetValueType.Previous: {
-      var previousKey = Keys.Where(k => k < level).OrderByDescending(k => k).FirstOrDefault();
-      if (previousKey != 0 || ContainsKey(previousKey)) {
-        return this[previousKey];
-      }
-      return defaultValue;
-    }
-    default:
-      return defaultValue;
-    }
-  }
-}
+public class LevelValueDictionary : IntKeyDictionary<int> { }
 
 }
