dc72cdf8d2ecc8f7e350290f2ad548e401d1b967
harada_takumi <limegame1004@gmail.com>
2024-12-18 22:12:54 +0900

    Add: CountCollectionの追加

---
+++ LimeLibrary/Scripts/Module/CountCollection/CountCollection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LimeLibrary.Module {
+
+[Serializable]
+public class CountCollection<T> {
+  [SerializeField]
+  private List<T> _list;
+
+  [SerializeField]
+  private int _count;
+
+  public void Initialize(List<T> list, int initialCount) {
+    _list = list;
+    _count = initialCount;
+  }
+
+  public List<T> GetList() {
+    var list = new List<T>();
+    for (int i = 0; i < _count; i++) {
+      list.Add(_list[i]);
+    }
+    return list;
+  }
+
+  public void AddCount(int count) {
+    _count += count;
+  }
+
+  public void Insert(T content) {
+    if (_list == null) return;
+    _list.Remove(content);
+    _list.Insert(_count, content);
+    _count++;
+  }
+
+  public bool IsUnlocked(T content) {
+    if (_list == null) return false;
+    int index = _list.IndexOf(content);
+    return index >= 0 && index < _count;
+  }
+
+  public void Clear() {
+    _list = null;
+    _count = 0;
+  }
+}
+
+}
