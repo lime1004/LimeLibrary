e5d7e35982c0796e13b50d76d81bba8635336fa2
harada_takumi <limegame1004@gmail.com>
2025-03-07 18:40:52 +0900

    Modify: AwakeUIAppEventの起動方法を追加

--- LimeLibrary/Scripts/UI/AwakeUIAppEvent.cs
+++ LimeLibrary/Scripts/UI/AwakeUIAppEvent.cs
@@ -1,32 +1,66 @@
 ﻿using System;
+using System.Threading;
 using Cysharp.Threading.Tasks;
-using LimeLibrary.Event;
 using LimeLibrary.Event.Core;
 using LimeLibrary.Extensions;
 using LimeLibrary.UI.App;
 using LimeLibrary.Utility;
 using UniRx;
+using UnityEngine;
 
 namespace LimeLibrary.UI {
 
-public class AwakeAppUIEvent : AbstractEvent {
+public class AwakeUIAppEvent : AbstractEvent {
+  private UIAppCreateType _createType;
   private string _address;
+  private GameObject _prefab;
+  private Func<CancellationToken, UniTask<UIApp>> _createFunc;
   private UIAppAwakeType _awakeType;
   private UniTask<UIApp> _createUIAppTask;
   private UniTask _finishUIAppTask;
   private UIApp _createdUIApp;
 
+  private enum UIAppCreateType {
+    Address,
+    Prefab,
+    Func,
+  }
+
   private readonly Subject<UIApp> _onCreateUIAppSubject = new();
   public IObservable<UIApp> OnCreateUIAppObservable => _onCreateUIAppSubject;
 
   public void Setup(string address, UIAppAwakeType awakeType) {
+    _createType = UIAppCreateType.Address;
     _address = address;
     _awakeType = awakeType;
   }
 
+  public void Setup(GameObject prefab, UIAppAwakeType awakeType) {
+    _createType = UIAppCreateType.Prefab;
+    _prefab = prefab;
+    _awakeType = awakeType;
+  }
+
+  public void Setup(Func<CancellationToken, UniTask<UIApp>> createFunc) {
+    _createType = UIAppCreateType.Func;
+    _createFunc = createFunc;
+  }
+
   public override void Start() {
-    // UI生成
-    _createUIAppTask = UIManager.Instance.UIAppManager.CreateAppAsync(_address, _awakeType, CancellationToken, true, false).RunHandlingError();
+    switch (_createType) {
+    case UIAppCreateType.Address:
+      _createUIAppTask = UIManager.Instance.UIAppManager.CreateAppAsync(_address, _awakeType, CancellationToken, true, false).RunHandlingError();
+      break;
+    case UIAppCreateType.Prefab:
+      _createUIAppTask = UIManager.Instance.UIAppManager.CreateAppAsync(_prefab, _awakeType, CancellationToken, true, false).RunHandlingError();
+      break;
+    case UIAppCreateType.Func:
+      _createUIAppTask = _createFunc.Invoke(CancellationToken).RunHandlingError();
+      break;
+    default:
+      Assertion.Assert(false);
+      break;
+    }
   }
 
   private enum Seq {
